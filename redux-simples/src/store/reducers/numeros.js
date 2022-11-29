265a96e6dfa1c7cd8bc9dc11cd5ddcf1acfe3bd6
Chrystian Ferreira <chrystian.profissional@gmail.com>
2022-11-29 20:22:21 -0300

    Seção 9 - Gerenciamento de estado com Redux

---
+++ redux-simples/src/store/reducers/numeros.js
@@ -0,0 +1,26 @@
+import { NUM_MIN_ALTERADO, NUM_MAX_ALTERADO } from "../actions/actionTypes"
+
+const INITIAL_STATE = {
+  min: 1,
+  max: 10
+}
+
+export default function(state = INITIAL_STATE, action) {
+  console.log("'numeros' reducer called...")
+  console.log("action", action)
+
+  switch(action.type) {
+    case NUM_MIN_ALTERADO:
+      return {
+        ...state,
+        min: action.payload
+      }
+    case NUM_MAX_ALTERADO:
+      return {
+        ...state,
+        max: action.payload
+      }
+    default: 
+      return state;
+  }
+}
